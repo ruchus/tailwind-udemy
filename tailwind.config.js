3141eb58cdbc89845a15611d6dd2a365de09c73c
ruchus <ruchus@gmail.com>
2022-07-20 19:43:47 +0200

    clase 12

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'body-mipatron-clase': "url('../img/pattern.png')",
+      }
+    },
   },
   plugins: [],
 }
